6fe74f36cf4479fe745adc9f57bcb01d04587789
Muchate <m7chate@gmail.com>
2024-09-12 08:16:35 +0200

    implementacao completa do try catch en todos inputs

--- T2_G1_L_Venda_e_Aluguel_de_imoveis/src/main/java/com/housesale/Main.java
+++ T2_G1_L_Venda_e_Aluguel_de_imoveis/src/main/java/com/housesale/Main.java
@@ -22,106 +22,132 @@ public class Main {
 
         int opc;
 
-        
-            while(true){
-
-                try {
-                    System.out.println("\n#############################################");
-                    System.out.println("\n- - - - Venda e arrendamento de casas - - - -");
-                    System.out.println("\n1 - Arrendar");
-                    System.out.println("\n2 - Cadastrar Casa");
-                    System.out.println("\n0 - Sair");
-                    opc = input.nextInt();
-                    if(opc==0){
-                        System.out.println("Fechando...");
-                    }
-                    
-
-                    switch (opc) {
-                        case 1:
-                            System.out.println("\n1 - Ver casas");
-                            System.out.println("\n2 - Cadastrar-se");
-                            System.out.println("\n0 - Voltar");
-                            int esc = input.nextInt();
-                            input.nextLine();
-        
-                            switch (esc) {
-                                case 1:
-                                    relacionamento.listarCasas();
-        
-                                    System.out.println("\n1 - Codigo da Casa a arrendar: ");
-                                    int cod = input.nextInt();
-        
-                                    relacionamento.arrendarCasar(cod);
-                                    break;
-                                case 2:
-                                    System.out.println("\nIntroduz o seu nome: ");
-                                    String nome = input.nextLine();
-                                    System.out.println("\nIntroduz o seu Email: ");
-                                    String email = input.next();
-                                    System.out.println("\nIntroduz o seu Telefone: ");
-                                    int telefone = input.nextInt();
-                                    System.out.println("\nIntroduz a sua morada: ");
-                                    String morada = input.nextLine();
-        
-                                    User usuario = new User(nome, email, telefone, morada);
-        
-                                    relacionamento.cadastrarUsuario(usuario);
-        
-                                    break;
-                                case 0:
-                                    break;
-                                default:
-                                    System.out.println("Opcao Invalida!");
-                                    break;
-                            }
-                            break;
-        
-                        case 2:
-                            String res = "";
-                            int i = 0;
-        
-                            System.out.println("Cadastre Casas e Clique - N - para terminar");
-                            do {
-                                System.out.println("\nInformacoes da " + (i + 1) + "ª Casa");
-                                System.out.println("\nTipo da Casa: ");
-                                String tipo_casa = input.nextLine();
-                                System.out.println("\nPreco: ");
-                                double preco = input.nextDouble();
-                                input.nextLine();
-                                System.out.println("\nLocalizacao: ");
-                                String localizacao = input.nextLine();
-        
-                                Casa casa = new Casa(preco, tipo_casa, localizacao);
-                                relacionamento.cadastrarCasas(casa);
-        
-                                relacionamento.listarUsuarios();
-        
-                                System.out.println("\nQuer Continuar? Y/N");
-                                res = input.next();
-        
-                                input.nextLine();
-        
-                                i++;
-                            } while (!res.equalsIgnoreCase("n"));
-                            break;
-        
-                        case 0:
-                            System.out.println("Saindo...");
-                            System.exit(0);
-                            break;
-        
-                        default:
-                            System.out.println("Opcao Invalida!");
-                            break;
-                    }
-                    
-                } catch (InputMismatchException e) {
-                    System.out.println("Introduza um numero inteiro! Tente novamente...");
-                    input.next();
+        while (true) {
+            try {
+                System.out.println("\n#############################################");
+                System.out.println("\n- - - - Venda e arrendamento de casas - - - -");
+                System.out.println("\n1 - Arrendar");
+                System.out.println("\n2 - Cadastrar Casa");
+                System.out.println("\n0 - Sair");
+                opc = input.nextInt();
+                if (opc == 0) {
+                    System.out.println("Fechando...");
+                    break;
                 }
+
+                switch (opc) {
+                    case 1:
+                        int esc;
+                        while (true) {
+                            try {
+                                System.out.println("\n1 - Ver casas");
+                                System.out.println("\n2 - Cadastrar-se");
+                                System.out.println("\n0 - Voltar");
+                                esc = input.nextInt();
+                                switch (esc) {
+                                    case 1:
+                                        relacionamento.listarCasas();
+
+                                        while (true) {
+                                            try {
+                                                System.out.println("\n1 - Codigo da Casa a arrendar: ");
+                                                int cod = input.nextInt();
+                                                relacionamento.arrendarCasar(cod);
+                                                break;
+                                            } catch (InputMismatchException e) {
+                                                System.out.println("Introduza um código válido! Tente novamente...");
+                                                input.next();
+                                            }
+                                        }
+                                        break;
+                                    case 2:
+                                        while (true) {
+                                            try {
+                                                System.out.println("\nIntroduz o seu nome: ");
+                                                String nome = input.nextLine();
+                                                System.out.println("\nIntroduz o seu Email: ");
+                                                String email = input.next();
+                                                System.out.println("\nIntroduz o seu Telefone: ");
+                                                int telefone = input.nextInt();
+                                                System.out.println("\nIntroduz a sua morada: ");
+                                                String morada = input.nextLine();
+
+                                                User usuario = new User(nome, email, telefone, morada);
+
+                                                relacionamento.cadastrarUsuario(usuario);
+                                                break;
+                                            } catch (InputMismatchException e) {
+                                                System.out.println("Introduza dados válidos! Tente novamente...");
+                                                input.next();
+                                            }
+                                        }
+                                        break;
+                                    case 0:
+                                        break;
+                                    default:
+                                        System.out.println("Opcao Invalida!");
+                                        break;
+                                }
+                                break;
+                            } catch (InputMismatchException e) {
+                                System.out.println("Introduza um número inteiro! Tente novamente...");
+                                input.next();
+                            }
+                        }
+
+                    case 2:
+                        while (true) {
+                            try {
+                                System.out.println("Cadastre Casas e Clique - N - para terminar");
+                                int i = 0;
+                                do {
+                                    System.out.println("\nInformacoes da " + (i + 1) + "ª Casa");
+                                    System.out.println("\nTipo da Casa: ");
+                                    String tipo_casa = input.nextLine();
+
+                                    System.out.println("\nPreco: ");
+                                    double preco = input.nextDouble();
+                                    input.nextLine();
+
+                                    System.out.println("\nLocalizacao: ");
+                                    String localizacao = input.nextLine();
+
+                                    Casa casa = new Casa(preco, tipo_casa, localizacao);
+                                    relacionamento.cadastrarCasas(casa);
+
+                                    relacionamento.listarUsuarios();
+
+                                    System.out.println("\nQuer Continuar? Y/N");
+                                    String resposta = input.next();
+
+                                    if (resposta.equalsIgnoreCase("y")) {
+                                        // continuar
+                                    } else if (resposta.equalsIgnoreCase("n")) {
+                                        break;
+                                    } else {
+                                        System.out.println("Resposta inválida! Tente novamente...");
+                                        continue;
+                                    }
+
+                                    i++;
+
+                                } while (true);
+                                break;
+                            } catch (InputMismatchException e) {
+                                System.out.println("Introduza dados válidos! Tente novamente...");
+                                input.next();
+                            }
+                        }
+                        break;
+
+                    default:
+                        System.out.println("Opcao Invalida!");
+                        break;
+                }
+            } catch (InputMismatchException e) {
+                System.out.println("Introduza um número inteiro! Tente novamente...");
+                input.next();
             }
-           
-        
+        }
     }
 }
